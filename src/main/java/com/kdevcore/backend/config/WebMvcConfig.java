ee1b57119a368b52d72a40b8d78482e7b05747ed
KDEVCORE <kdevcore@gmail.com>
2023-04-04 15:29:07 +0900

    line sort

--- src/main/java/com/kdevcore/backend/config/WebMvcConfig.java
+++ src/main/java/com/kdevcore/backend/config/WebMvcConfig.java
@@ -8,16 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
     private final String DEV_URL = "http://localhost:3000";
-    private final String PROD_HTTP_URL = "http://app.kdevcore.com";
     private final String PROD_HTTPS_URL = "https://app.kdevcore.com";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins(DEV_URL, PROD_HTTP_URL, PROD_HTTPS_URL)
-            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
-            .allowCredentials(true)
-            .maxAge(MAX_AGE_SECS);
+                .allowedOrigins(DEV_URL, PROD_HTTPS_URL)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(MAX_AGE_SECS);
     }
 }
