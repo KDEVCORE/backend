e86d9a40c0de4e999fd0cdb5b0ec207fe4a9e6d0
KDEVCORE <kdevcore@gmail.com>
2023-04-04 18:45:52 +0900

    root domain url added

--- src/main/java/com/kdevcore/backend/config/WebMvcConfig.java
+++ src/main/java/com/kdevcore/backend/config/WebMvcConfig.java
@@ -9,11 +9,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private final long MAX_AGE_SECS = 3600;
     private final String DEV_URL = "http://localhost:3000";
     private final String PROD_HTTPS_URL = "https://app.kdevcore.com";
+    private final String PROD_HTTPS_ROOT_URL = "https://kdevcore.com";
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(DEV_URL, PROD_HTTPS_URL)
+                .allowedOrigins(DEV_URL, PROD_HTTPS_URL, PROD_HTTPS_ROOT_URL)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
