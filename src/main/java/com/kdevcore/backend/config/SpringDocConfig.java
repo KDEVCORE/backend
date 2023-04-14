b5fa00694bc9c2289dd4149a2fddda6ca5f33af9
KDEVCORE <kdevcore@gmail.com>
2023-04-14 17:36:44 +0900

    spring doc annotation remove

--- src/main/java/com/kdevcore/backend/config/SpringDocConfig.java
+++ src/main/java/com/kdevcore/backend/config/SpringDocConfig.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -26,18 +25,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
         @Tag(name = "Member", description = "Member process"),
         @Tag(name = "Todo", description = "Todo process"),
         @Tag(name = "Undefined", description = "Undefined process"),
-    },
-    security = {
-        @SecurityRequirement(name = "X-Auth-Token"),
     }
 )
-@SecuritySchemes({
-    @SecurityScheme(name = "X-Auth-Token",
-                    type = SecuritySchemeType.APIKEY,
-                    description = "API token",
-                    in = SecuritySchemeIn.HEADER,
-                    paramName = "X-Auth-Token"),
-})
 @Configuration
 public class SpringDocConfig {
 }
