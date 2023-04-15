0e4f357eee1afce3ad66850a2632f53115cb1883
KDEVCORE <kdevcore@gmail.com>
2023-04-15 16:30:08 +0900

    open api definition moved

--- src/main/java/com/kdevcore/backend/BackendApplication.java
+++ src/main/java/com/kdevcore/backend/BackendApplication.java
@@ -3,6 +3,26 @@ package com.kdevcore.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "KDEVCORE application API",
+        description = "List of api rules used in this project",
+        version = "0.1.230414",
+        contact = @Contact(
+                    name = "developer (kdevcore)",
+                    email = "kdevcore@gmail.com")
+    ),
+    tags = {
+        @Tag(name = "Member", description = "Member process"),
+        @Tag(name = "Todo", description = "Todo process"),
+        @Tag(name = "Undefined", description = "Undefined process"),
+    }
+)
 @SpringBootApplication
 public class BackendApplication {
 
