cadac8d47970b8a4c943f8298b804ce196bda979
KDEVCORE <kdevcore@gmail.com>
2023-04-15 16:25:14 +0900

    unnecessary import remove

--- src/main/java/com/kdevcore/backend/model/TodoEntity.java
+++ src/main/java/com/kdevcore/backend/model/TodoEntity.java
@@ -3,7 +3,6 @@ package com.kdevcore.backend.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
