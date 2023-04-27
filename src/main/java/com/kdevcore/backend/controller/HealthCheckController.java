21744cd07d9aa85fff78fb061205cdb0f4800b22
KDEVCORE <kdevcore@gmail.com>
2023-04-27 16:32:02 +0900

    api tag name modified

--- src/main/java/com/kdevcore/backend/controller/HealthCheckController.java
+++ src/main/java/com/kdevcore/backend/controller/HealthCheckController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Tag(name = "Undefined")
+@Tag(name = "Health Check")
 @RestController
 public class HealthCheckController {
-    @Operation(summary = "check running server", description = "서버가 실행중인지 확인하는 용도")
+    @Operation(summary = "Check running server", description = "서버가 실행중인지 확인하는 용도")
     @GetMapping("/health")
     public String healthCheck() {
         return "The service is up and running...";
