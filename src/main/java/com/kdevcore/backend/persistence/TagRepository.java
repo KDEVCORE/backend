999ded54cfd4ebac29d4823c304878421a39e28e
KDEVCORE <kdevcore@gmail.com>
2023-05-11 11:14:12 +0900

    post design destruction

--- src/main/java/com/kdevcore/backend/persistence/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kdevcore.backend.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kdevcore.backend.model.TagEntity;
-
-
-public interface TagRepository extends JpaRepository<TagEntity, String> {
-}
